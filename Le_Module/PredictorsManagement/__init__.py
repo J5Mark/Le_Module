d16013c05cbce3952b530da2b20a66bca0388601
J5Mark <64222034+J5Mark@users.noreply.github.com>
2024-01-13 01:34:43 +0300

    Delete Le_Module directory

--- Le_Module/PredictorsManagement/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['Factories', 'Predictors', 'old']
--- Le_Module/StrategiesManagement/RiskManagers.py
+++
@@ -1,9 +0,0 @@
-from PredictorsManagement.Predictors import Predictor
-from Utilities.FinUtils import *
-
-class FallPerceptionRiskManager(Predictor):
-    def __init__(self, border_fall: float):
-        self.border_fall = border_fall
-    
-    def predict(self, databit: Candles, avg_buy_price: float) -> bool:
-        return databit[-1].Close.values <= avg_buy_price*(1-self.border_fall)
