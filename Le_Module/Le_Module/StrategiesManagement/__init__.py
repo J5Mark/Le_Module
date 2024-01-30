4d1d7eda004287977e8aaf2455851fc7324ddb5a
J5Mark <64222034+J5Mark@users.noreply.github.com>
2024-01-30 20:15:30 +0300

    Delete Le_Module/Le_Module directory
    IDK why i created LeModule inside a LeModule this is stupid and accidental

--- Le_Module/Le_Module/StrategiesManagement/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['DecidingModules', 'QuantityControllers', 'RiskManagers', 'sm', 'old']
--- Le_Module/Le_Module/setup.py
+++
@@ -1,3 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='Le_Module', version='1.0.2', packages=find_packages())
